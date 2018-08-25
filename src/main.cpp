eea094997e26896c3a60a1f7f73c474e1d0380b2
Rob Berwick <rob.berwick@gmail.com>
2018-08-25 19:54:22 +0100

    switch to dfplayer

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include "InputDebounce.h"
-#include "WTV020SD16P.h"
+#include "DFRobotDFPlayerMini.h"
 
 #define BUTTON_DEBOUNCE_DELAY   20   // [ms]
 
 // pin definitions
 static const int pinLED = LED_BUILTIN; // 13
 static const int AUDIO_TRIGGER_PIN = 12;
-static const int wtvClockPin = 11;  // The pin number of the clock pin.
-static const int wtvDataPin = 10;  // The pin number of the data pin.
-static const int wtvBusyPin = 9;  // The pin number of the busy pin.
-static const int wtvBusyCallbackPin = 8;  // The pin number of the busy pin.
 
 // state variables
 bool audioIsOn = false;
@@ -21,15 +18,17 @@ static InputDebounce audioTriggerButton;
 static InputDebounce audioBusyPin;
 
 // MP3 player module
-static WTV020SD16P wtv020sd16p(wtvClockPin, wtvDataPin, wtvBusyPin);
-// audio file looping array. using selectedAudioFile as an index, this array
+// audio file loopin
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+SoftwareSerial Serial1(10, 11); // RX, TXg array. using selectedAudioFile as an index, this array
 // determines whether or not the file should be looped
 const int numAudioFiles = 3;
 const bool audioLoop [] = {true, true, false};
 
 long getRandomAudioFile()
 {
-  return 0;
+  return 1;
   // return random(0, numAudioFiles);
 }
 
@@ -73,11 +72,11 @@ void debouncedButtonReleasedCallback(uint8_t pinIn)
     Serial.println((shouldAudioLoop()) ? "Yes" : "No");
     if (audioIsOn)
     {
-      wtv020sd16p.asyncPlayVoice(selectedAudioFile);
+      myDFPlayer.play(selectedAudioFile);
     } else {
       setSelectedAudioFile(-1);
       audioIsOn = false;
-      wtv020sd16p.stopVoice();
+      myDFPlayer.stop();
     }
   }
 }
@@ -102,12 +101,92 @@ void debouncedButtonReleasedDurationCallback(uint8_t pinIn, unsigned long durati
   Serial.println("ms");
 }
 
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
+}
+
 void setup() {
     // initialize digital pin as an output
   pinMode(pinLED, OUTPUT);
 
   // init serial
-  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  //Use softwareSerial to communicate with mp3.
+  if (!myDFPlayer.begin(Serial1)) {
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  // myDFPlayer.play(1);  //Play the first mp3
 
   // register callback functions (shared, used by all buttons)
   audioTriggerButton.registerCallbacks(debouncedButtonPressedCallback, debouncedButtonReleasedCallback, debouncedButtonPressedDurationCallback, debouncedButtonReleasedDurationCallback);
@@ -115,8 +194,6 @@ void setup() {
 
   // setup input buttons (debounced)
   audioTriggerButton.setup(AUDIO_TRIGGER_PIN, BUTTON_DEBOUNCE_DELAY);
-  pinMode(wtvBusyCallbackPin, INPUT);
-  audioBusyPin.setup(wtvBusyCallbackPin, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_EXT_PULL_DOWN_RES);
 
 }
 
@@ -126,6 +203,10 @@ void loop() {
     // poll button state
     audioTriggerButton.process(now);
     audioBusyPin.process(now);
+
+    if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
     delay(1); // [ms]
 
 }
